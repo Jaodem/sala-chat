d12a6da5e825b62bd798bdc047e49a4de696b023
Jao <109613248+Jaodem@users.noreply.github.com>
2025-06-29 16:07:46 -0300

    Inicializa proyecto backend: configuración base, dependencias y archivo server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
+// Rutas
+const authRoutes = require('./routes/auth');
+const chatRoutes = require('./routes/chat');
+
+// Inicializacions
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: '*'}
+});
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Endpoints
+app.use('/api/auth', authRouthes);
+app.use('api/chat', chatRoutes);
+
+// Sockets
+const chatSocket = require('./sockets/chatSocket');
+io.on('connection', (socket) => chatSocket(socket, io));
+
+// Conexión a Mongo y servidor
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        server.listen(process.env.PORT, () =>
+            console.log(`Servidor corriendo en puerto ${process.PORT}`)
+        );
+    })
+    .catch(err => console.error('Error al conectar a MongoDB', err));
